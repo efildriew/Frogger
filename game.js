d497796a45afa55a6b403345416b4326da4fb06e
Marc <13a1911@gmail.com>
2019-07-02 21:01:08 +0200

    empezada clase game. definidas propiedades de clase game y dibujo de tablero MVP

---
+++ game.js
@@ -0,0 +1,18 @@
+class Game {
+  constructor(options) {
+    this.rows = options.rows;
+    this.columns = options.columns;
+    this.maxCells = optsions.maxCells;
+    this.ctx = options.ctx;
+    this.froggy = options.froggy;
+    this.gameOver = undefined;
+  }
+
+  _drawBoard() {
+    this.ctx.fillStyle = "#000000";
+    this.ctx.fillRect(0, 0, 600, 800);
+    this.ctx.fillStyle = "#FF7700";
+    this.ctx.fillRect(0, 0, 600, 100);
+    this.ctx.fillRect(0, 700, 600, 800);
+  }
+}
