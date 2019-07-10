2d317df6ea2583f7dbaaeff8185a40ea287dd0c9
Marc Valles Casares <13a1911@gmail.com>
2019-07-10 18:08:27 +0200

    en la pantalla inicial, puesto el título, el modo de empezar, y una frase

--- intro.js
+++ intro.js
@@ -14,11 +14,27 @@ document.onload = (function() {
 
   ctx.fillStyle = "#00FF00";
   ctx.fillRect(50, 50, 500, 200);
+  ctx.font = "130px Arial";
+  ctx.strokeText("FROGGY", 70, 180, 460);
 
   // Dibuja el rectángulo donde irá el "Insert Coin"
 
   ctx.fillStyle = "#CC6600";
   ctx.fillRect(200, 600, 200, 50);
+  ctx.font = "40px Arial";
+  ctx.strokeText("Press 1 to start!", 210, 638, 180);
+
+  // Credito
+
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.fillText(
+    "A tribute by Marc Valles",
+    canvas.width / 2,
+    canvas.height / 2,
+    200
+  );
 
   // Definir tecla 1 para empezar el juego
 
